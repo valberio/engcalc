10262ab4af57c84532cff60f6b66f7f88f8622e4
Valen <valberio@frba.utn.edu.ar>
2023-09-24 19:14:04 -0300

    A lot of advancements

--- matrix_calculator.js
+++ matrix_calculator.js
@@ -54,30 +54,59 @@ document.addEventListener("DOMContentLoaded", () => {
 
     document.querySelector("input").addEventListener("keydown", handleArrowKeys);
 
-    document.querySelector("#matrixInputsA .add-row").addEventListener("click", () => {
+    document.getElementById("add-row-A").addEventListener("click", () => {
         const rowCount = matrixInputsContainerA.childElementCount + 1;
         const colCount = matrixInputsContainerA.querySelector(".matrix-input-row")?.childElementCount || 2;
         const inputRow = createInputRow(rowCount, colCount);
         matrixInputsContainerA.appendChild(inputRow);
     });
 
-    document.querySelector("#matrixInputsA .add-column").addEventListener("click", () => {
+
+    document.getElementById("add-col-A").addEventListener("click", () => {
         const colCount = matrixInputsContainerA.querySelector(".matrix-input-row")?.childElementCount + 1 || 2;
         const inputRows = matrixInputsContainerA.querySelectorAll(".matrix-input-row");
         inputRows.forEach(row => {
             const input = document.createElement("input");
+            input.value = 0;
+            input.classList.add("col");
+            row.appendChild(input);
+        });
+    });
+
+    document.getElementById("add-row-B").addEventListener("click", () => {
+        const rowCount = matrixInputsContainerB.childElementCount + 1;
+        const colCount = matrixInputsContainerB.querySelector(".matrix-input-row")?.childElementCount || 2;
+        const inputRow = createInputRow(rowCount, colCount);
+        matrixInputsContainerB.appendChild(inputRow);
+    });
+
+    document.getElementById("add-col-B").addEventListener("click", () => {
+        const colCount = matrixInputsContainerB.querySelector(".matrix-input-row")?.childElementCount + 1 || 2;
+        const inputRows = matrixInputsContainerB.querySelectorAll(".matrix-input-row");
+        inputRows.forEach(row => {
+            const input = document.createElement("input");
+            input.value = 0;
+            input.classList.add("col");
             row.appendChild(input);
         });
     });
 
     document.getElementById("additionButton").addEventListener("click", () => {performAddition();})
-    document.getElementById("substractionButton").addEventListener("click", () => {performSubstraction();})
-    document.getElementById("multiplicationButton").addEventListener("click", () => {performMultiplication();})
+                                                                                    
+    document.getElementById("substractionButton").addEventListener("click", function() {
+        performSubstraction();
+        checkEqualRowsAndCols(getMatrixA(), getMatrixB());
+    })
+    document.getElementById("multiplicationButton").addEventListener("click", function() {
+        performMultiplication();
+        checkEqualRowsAndCols(getMatrixA(), getMatrixB());
+    })
 
     function handleArrowKeys(event) {
         const input = event.target;
         const row = input.closest(".matrix-input-row");
         const rowInputs = Array.from(row.querySelectorAll("input"));
+        const rows = document.querySelectorAll("matrix-input-row");
         const currentIndex = rowInputs.indexOf(input);
         let nextIndex = currentIndex;
 
@@ -94,9 +123,11 @@ document.addEventListener("DOMContentLoaded", () => {
             console.log("Arriba presionado");
         } else if (event.key === "ArrowDown") {
             const rowIndex = Array.from(matrixInputsContainerA.children).indexOf(row);
+            console.log(`Apretaste abajo en la fila ${rowIndex}`);
             const nextRow = matrixInputsContainerA.children[rowIndex + 1];
             if (nextRow) {
-                nextIndex = Math.min(nextRow.querySelectorAll("input").length - 1, currentIndex);
+                //nextIndex = Math.min(nextRow.querySelectorAll("input").length - 1, currentIndex);
+                console.log("entre");
             }
             console.log("Abajo presionado");
         }
@@ -107,28 +138,50 @@ document.addEventListener("DOMContentLoaded", () => {
     function performAddition(){
         const A = getMatrixA();
         const B = getMatrixB();
-        
-        const result = addMatrixes(A, B);
-        console.log(`El resultado de la operacion es ${result}`);
-        showMatrixOnResult(result);
+        canOperate = checkEqualRowsAndCols(A, B);
+
+        if (canOperate){
+            const result = addMatrixes(A, B);
+            console.log(`El resultado de la operacion es ${result}`);
+            showMatrixOnResult(result);
+        }
+        else{
+            alert('The matrixes should be of the same dimension!');
+        }
     }
 
     function performSubstraction(){
-        const a = getMatrixA();
-        const b = getMatrixB();
+        const A = getMatrixA();
+        const B = getMatrixB();
 
-        const result = substractMatrixes(a, b);
-        console.log(`El resultado de la resta es ${result}`);
-        showMatrixOnResult(result);
+        canOperate = checkEqualRowsAndCols(A, B);
+
+        if (canOperate){
+            const result = substractMatrixes(A, B);
+            console.log(`El resultado de la resta es ${result}`);
+            showMatrixOnResult(result);
+        }
+        else{
+            alert('The matrixes should be of the same dimension!');
+        }
+
     }
 
     function performMultiplication(){
-        const a = getMatrixA();
-        const b = getMatrixB();
+        const A = getMatrixA();
+        const B = getMatrixB();
 
-        const result = multiplyMatrixes(a, b);
-        console.log(`El resultado de la multiplicacion es ${result}`);
-        showMatrixOnResult(result);
+        canOperate = checkEqualRowsAndCols(A, B);
+
+        if (canOperate){
+            const result = multiplyMatrixes(A, B);
+            console.log(`El resultado de la multiplicacion es ${result}`);
+            showMatrixOnResult(result);
+        }
+        else{
+            alert('The matrixes should be of the same dimension!');
+        }
+
     }
 
     function getMatrixA(){
@@ -236,8 +289,20 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     function ammountOfRows(matrix){
-        
+        return matrix.length;
     }
-});
+    function ammountOfCols(matrix){
+        return matrix[0].length;
+    }
+
+    function checkEqualRowsAndCols(matrixA, matrixB){
+        console.log(`Matrix A has ${ammountOfRows(matrixA)} rows and ${ammountOfRows(matrixB)} cols`);
+        console.log(`Matrix B has ${ammountOfRows(matrixB)} rows and ${ammountOfCols(matrixB)} cols`);
+        
+        canOperationsBePerfomed = ((ammountOfRows(matrixA) == ammountOfRows(matrixB)) && (ammountOfCols(matrixA) == ammountOfCols(matrixB)));
+        console.log(`Can operations be perfomed: ${canOperationsBePerfomed}`);
+        return canOperationsBePerfomed;
+    }
+})
 
 //TO DO: dimension checks
